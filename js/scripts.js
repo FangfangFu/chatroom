6c6f11997ae8718d4e88d7778987509e4e824c25
Fangfang Fu <fangfang@fufangfang.com>
2016-01-09 22:57:12 -0600

    Add messages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 $('#title').addClass('redtitle');
+
+var json = {
+   "room":{
+      "name":"FangDev",
+      "members":"Salgat:1452401384430,Fangfang2:1452401457085",
+      "messages":[
+         "Salgat: I love you more xiaobei",
+         "Fangfang: I love you Laobei"
+      ]
+   }
+};
+
+var messages = json.room.messages;
+var arrayLength = messages.length;
+for (var i = 0; i < arrayLength; i++) {
+    $('#messages').find('ul').append('<li>' + messages[i] + '</li>');
+}
