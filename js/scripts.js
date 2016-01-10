31bfcff99d1aace90a7d5239846d6297ff04a669
Fangfang Fu <fangfang@fufangfang.com>
2016-01-10 00:07:50 -0600

    Add input and submit

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,28 @@ var json = {
    }
 };
 
-var messages = json.room.messages;
-var arrayLength = messages.length;
-for (var i = 0; i < arrayLength; i++) {
-    $('#messages').find('ul').append('<li>' + messages[i] + '</li>');
+function removeMessages(){
+    $('li').remove();
 }
+
+function showMessages(messages){
+    var arrayLength = messages.length;
+    for (var i = 0; i < arrayLength; i++) {
+        $('#messages').find('ul').append('<li>' + messages[i] + '</li>');
+    } 
+}
+
+window.setInterval(function(){
+  $.post( "http://singleendpointchatserver.herokuapp.com/api/v1/chatroom/FangDev?username=Fangfang&expireafter=60", function( json ) {
+    var messages = json.room.messages;
+    removeMessages();
+    showMessages(messages);
+  });
+}, 1000);
+
+$('#messageSubmit').on('click', function(event) {
+  var message = $('#messageInput').val();
+  console.log("Button pressed with value: " + message);
+  var encodedMessage = encodeURIComponent(message);
+  $.post( "http://singleendpointchatserver.herokuapp.com/api/v1/chatroom/FangDev?username=Fangfang&message="+encodedMessage+"&expireafter=60");
+});
